90cd7b245d966bad33c02f56f66902683b7dcbb6
Victor Ribeiro de Lima <victor@DESKTOP-4VLO0U0.>
2025-04-05 01:08:39 -0300

    feat: version v.1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="payment-plan-python-sdk",
-    version="1.0.0",
+    version="1.0.1",
     description="A Python SDK for Lara Payment Plan, a wrapper around the Lara Payment Plan Rust library.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
